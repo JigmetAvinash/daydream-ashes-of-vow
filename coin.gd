b0e6b1058caafc330d1c26fb82d77ea486d5c5a5
Jigmet Avinash Kumar <javinashkumar14@gmail.com>
2025-10-05 10:44:12 +0530

    Added Coin Addition System

--- coin.gd
+++ coin.gd
@@ -2,5 +2,10 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	print("+1 Coin")
-	queue_free()
+	print("Coin touched by: ", body.name)
+	if body.is_in_group("Player"):
+		print("Player detected! Adding coin...")
+		CoinGameState.add_coin()
+		queue_free()
+	else:
+		print("Not player, groups: ", body.get_groups())
